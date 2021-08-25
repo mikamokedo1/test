a6a0c4bd69b515723fdd79cc1184e6eb766f0919
Le Quy <quy.le@gotrust.vn>
2021-08-25 13:25:50 +0700

    first commit

---
+++ src/@crema/services/auth/jwt-auth/jwt-api.tsx
@@ -0,0 +1,29 @@
+import axios from 'axios';
+const BaseAPI = process.env.REACT_APP_API_URL;
+const jwtAxios = axios.create({
+  baseURL: BaseAPI, 
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+jwtAxios.interceptors.response.use(
+  res => res,
+  err => {
+    if (err.response && err.response.data.msg === 'Token is not valid') {
+      console.log('Need to logout user');
+      // store.dispatch({type: LOGOUT});
+    }
+    return Promise.reject(err);
+  },
+);
+export const setAuthToken = (token: string | null) => {
+  if (token) {
+    jwtAxios.defaults.headers.common['x-auth-token'] = token;
+    localStorage.setItem('token', token);
+  } else {
+    delete jwtAxios.defaults.headers.common['x-auth-token'];
+    localStorage.removeItem('token');
+  }
+};
+
+export default jwtAxios;
